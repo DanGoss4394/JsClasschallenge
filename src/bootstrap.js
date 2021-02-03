5396617d330f1bba331bf7684660088b88c559f8
Daniel <dangoscode@gmail.com>
2021-02-03 10:16:27 -0700

    deleted a file

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,9 +1,26 @@
 import classChallenge, { greeting, array } from "./classChallenge";
-import { string, join } from "./codeChallange";
 
 console.log(classChallenge());
 console.log(greeting);
 console.log(array());
-console.log(string())
-console.log(join());
 
+function string(str) {
+  str = str.split(" ");
+  const newArray1 = [];
+  for (let i = 0; i < str.length; i++) {
+    newArray1.push(str[i].charAt(0).toUpperCase() + str[i].slice(1));
+  }
+  return newArray1.join(" ");
+}
+console.log(string("the quick brown fox"));
+
+function join(arrOne, arrTwo) {
+  const maxLength = Math.max(arrOne.length, arrTwo.length);
+  let result = [];
+  for (let i = 0; i < maxLength; i++) {
+    result.push(arrOne[i]);
+    result.push(arrTwo[i]);
+  }
+  return result.filter((value) => value !== undefined);
+}
+console.log(join(["a", "b", "c"], [1, 2, 3]));
