4db7171af343429a60989750c445dd0fb7fda7ab
Daniel <dangoscode@gmail.com>
2021-02-03 09:01:26 -0700

    init commit

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,9 @@
 import classChallenge, { greeting, array } from "./classChallenge";
+import { string, join } from "./codeChallange";
 
 console.log(classChallenge());
 console.log(greeting);
 console.log(array());
+console.log(string())
+console.log(join());
+
